66e9f93f94d5179064256adf2efdbe908e43f3e8
Alexandr Kirichuk <kirichuc@gmail.com>
2025-08-25 16:27:57 +0300

    rescheduling a meeting

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -19,7 +19,7 @@ public class CardDeliveryOrderTest {
 
     @Test
     public void autoDataGeneration() {
-        var date = DataGenerator.generateDate(3);
+        var date = DataGenerator.generateDate(5);
         var name = DataGenerator.generateName(new Faker());
         var phone = DataGenerator.generatePhone(new Faker());
         $("[data-test-id='city'] input").setValue(DataGenerator.generateCity());
@@ -32,24 +32,11 @@ public class CardDeliveryOrderTest {
         $$(".button").find(Condition.text("Запланировать")).click();
         $("[data-test-id='success-notification']")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + date));
-    }
-
- /*   @Test
-    public void rescheduleMeetingTime() {
-        var date = DataGenerator.generateDate(3);
-        $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(date);
-        $("[data-test-id='name'] input").setValue("Петров Иван");
-        $("[data-test-id='phone'] input").setValue("+79000000000");
-        $("[data-test-id='agreement'").click();
         $$(".button").find(Condition.text("Запланировать")).click();
-        $("[data-test-id='replan-notification']").shouldBe(Condition.visible);
         $("[data-test-id='replan-notification']")
                 .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $$(".button").find(Condition.text("Перепланировать")).click();
         $("[data-test-id='success-notification']")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + date));
-    }*/
+    }
 }
